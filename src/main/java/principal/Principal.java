23ae1ca1df977e4ad3f84f1c7c2aef1174815c96
JP-Becker <jpbecker20@gmail.com>
2024-05-13 11:34:45 -0300

    Instanciando o menu na classe Principal para aparecer ao dar run

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,7 +1,11 @@
 package principal;
 
+import view.FrmMenuPrincipal;
+
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("oi!");
+        // instanciando o objeto FrmMenuPrincipal e setando visible como true para aparecer
+        FrmMenuPrincipal objetoTela = new FrmMenuPrincipal();
+        objetoTela.setVisible(true);
     }
 }
