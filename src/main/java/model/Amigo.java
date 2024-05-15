f8e46e0f12c2e73af3ea9ce500dd44a568d81b22
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-05-14 23:44:08 -0300

    feat: Implementada em Amigo e Ferramenta interação com DAO
    Adição de métodos nas classes Amigo e Ferramenta que referenciam seus respectivos DAOs
    
    (Obs: Os métodos referenciais dependem de métodos de DAO que ainda não foram implementados, então esses métodos referenciais não são funcionais ainda, e devem permanecer comentados até a implementação dos métodos DAO para evitar avisos de erro na IDE)

--- src/main/java/model/Amigo.java
+++ src/main/java/model/Amigo.java
@@ -1,87 +1,126 @@
 package model;
 
+import dao.FerramentaDAO;
+import java.util.ArrayList;
+
 public class Amigo {
 
-    private int idAmigo;
-    private String nomeAmigo;
-    private String telefone;
-    private int emprestimosTotais;
-    private int emprestimosAtivos;
+	private int idAmigo;
+	private String nomeAmigo;
+	private String telefone;
+	private int emprestimosTotais;
+	private int emprestimosAtivos;
 
-    public Amigo() {
-        this(0, "", "", 0, 0);
-    }
+	private FerramentaDAO dao;
 
-    // Construtor com valor 0 nos emprestimos já que todo amigo começa sem emprestimos
-    public Amigo(int idAmigo, String nomeAmigo, String telefone) {
-        this.idAmigo = idAmigo;
-        this.nomeAmigo = nomeAmigo;
-        this.telefone = telefone;
-        emprestimosTotais = 0;
-        emprestimosAtivos = 0;
-    }
+	public Amigo() {
+		this(0, "", "", 0, 0);
+	}
 
-    public Amigo(int idAmigo, String nomeAmigo, String telefone, int emprestimosTotais, int emprestimosAtivos) {
-        this.idAmigo = idAmigo;
-        this.nomeAmigo = nomeAmigo;
-        this.telefone = telefone;
-        this.emprestimosTotais = emprestimosTotais;
-        this.emprestimosAtivos = emprestimosAtivos;
-    }
+	// Construtor com valor 0 nos emprestimos já que todo amigo começa sem emprestimos
+	public Amigo(int idAmigo, String nomeAmigo, String telefone) {
+		this.idAmigo = idAmigo;
+		this.nomeAmigo = nomeAmigo;
+		this.telefone = telefone;
+		emprestimosTotais = 0;
+		emprestimosAtivos = 0;
+	}
 
-    public int getIdAmigo() {
-        return idAmigo;
-    }
+	public Amigo(int idAmigo, String nomeAmigo, String telefone, int emprestimosTotais, int emprestimosAtivos) {
+		this.idAmigo = idAmigo;
+		this.nomeAmigo = nomeAmigo;
+		this.telefone = telefone;
+		this.emprestimosTotais = emprestimosTotais;
+		this.emprestimosAtivos = emprestimosAtivos;
+	}
 
-    public void setIdAmigo(int idAmigo) {
-        this.idAmigo = idAmigo;
-    }
+	public int getIdAmigo() {
+		return idAmigo;
+	}
 
-    public String getNomeAmigo() {
-        return nomeAmigo;
-    }
+	public void setIdAmigo(int idAmigo) {
+		this.idAmigo = idAmigo;
+	}
 
-    public void setNomeAmigo(String nomeAmigo) {
-        this.nomeAmigo = nomeAmigo;
-    }
+	public String getNomeAmigo() {
+		return nomeAmigo;
+	}
 
-    public String getTelefone() {
-        return telefone;
-    }
+	public void setNomeAmigo(String nomeAmigo) {
+		this.nomeAmigo = nomeAmigo;
+	}
 
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
+	public String getTelefone() {
+		return telefone;
+	}
 
-    public int getEmprestimosTotais() {
-        return emprestimosTotais;
-    }
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
 
-    public void setEmprestimosTotais(int emprestimosTotais) {
-        this.emprestimosTotais = emprestimosTotais;
-    }
+	public int getEmprestimosTotais() {
+		return emprestimosTotais;
+	}
 
-    public int getEmprestimosAtivos() {
-        return emprestimosAtivos;
-    }
+	public void setEmprestimosTotais(int emprestimosTotais) {
+		this.emprestimosTotais = emprestimosTotais;
+	}
 
-    public void setEmprestimosAtivos(int emprestimosAtivos) {
-        this.emprestimosAtivos = emprestimosAtivos;
-    }
+	public int getEmprestimosAtivos() {
+		return emprestimosAtivos;
+	}
 
-    @Override
-    public String toString() {
-        return "idAmigo=" + idAmigo + ", nomeAmigo=" + nomeAmigo + ", telefone=" + telefone + ", emprestimosTotais=" + emprestimosTotais;
-    }
+	public void setEmprestimosAtivos(int emprestimosAtivos) {
+		this.emprestimosAtivos = emprestimosAtivos;
+	}
 
-    public String limparTelefone(String telefone) {
+	@Override
+	public String toString() {
+		return "idAmigo=" + idAmigo + ", nomeAmigo=" + nomeAmigo + ", telefone=" + telefone + ", emprestimosTotais=" + emprestimosTotais;
+	}
 
-        telefone = telefone.replace("-", "");
-        telefone = telefone.replace("(", "");
-        telefone = telefone.replace(")", "");
-        telefone = telefone.replace(" ", "");
-        return telefone;
+	public String limparTelefone(String telefone) {
+		telefone = telefone.replace("-", "");
+		telefone = telefone.replace("(", "");
+		telefone = telefone.replace(")", "");
+		telefone = telefone.replace(" ", "");
+		return telefone;
+	}
 
-    }
+	// Os métodos a seguir referenciam implementacoes futuras da classe DAO
+	// Pro netbeans não reclamar, mantenha esse trecho comentado por enquanto
+	
+	/*
+	public ArrayList<Amigo> getMinhaLista() {
+		return dao.getMinhaLista();
+	}
+
+	public boolean insertAmigoBD(String nome, String telefone, int emprestimosTotais, int emprestimosAtivos) {
+		int id = this.maiorID() + 1;
+		Amigo objeto = new Amigo(id, nome, telefone, emprestimosTotais, emprestimosAtivos);
+		dao.insertAmigoBD(objeto);
+		return true;
+	}
+
+	public boolean deleteAmigoBD(int id) {
+		dao.deleteAmigoBD(id);
+		return true;
+	}
+
+	public boolean updateAmigoBD(int id, String nome, String telefone, int emprestimosTotais, int emprestimosAtivos) {
+		Amigo objeto = new Amigo(id, nome, telefone, emprestimosTotais, emprestimosAtivos);
+		dao.updateAmigoBD(objeto);
+		return true;
+	}
+
+	public Amigo carregaAmigo(int id) {
+		return dao.carregaAmigo(id);
+	}
+
+	public int maiorID() {
+		return dao.maiorID();
+	}
+
+	*/
 
 }
--- src/main/java/model/Ferramenta.java
+++ src/main/java/model/Ferramenta.java
@@ -1,77 +1,118 @@
 package model;
 
+import dao.FerramentaDAO;
+import java.util.ArrayList;
+
 public class Ferramenta {
 
-    private int idFerramenta;
-    private String nomeFerramenta;
-    private String marca;
-    private double custo;
-    private boolean emprestada;
+	private int idFerramenta;
+	private String nomeFerramenta;
+	private String marca;
+	private double custo;
+	private boolean emprestada;
 
-    public Ferramenta() {
-        this(0, "", "", 0.0, false);
-    }
+	private FerramentaDAO dao;
 
-    // Construtor com valor false no boolean emprestada já que toda ferramenta começa não emprestada
-    public Ferramenta(int idFerramenta, String nomeFerramenta, String marca, double custo) {
-        this.idFerramenta = idFerramenta;
-        this.nomeFerramenta = nomeFerramenta;
-        this.marca = marca;
-        this.custo = custo;
-        this.emprestada = false;
-    }
-    
-    public Ferramenta(int idFerramenta, String nomeFerramenta, String marca, double custo, boolean emprestada) {
-        this.idFerramenta = idFerramenta;
-        this.nomeFerramenta = nomeFerramenta;
-        this.marca = marca;
-        this.custo = custo;
-        this.emprestada = emprestada;
-    }
+	public Ferramenta() {
+		this(0, "", "", 0.0, false);
+	}
 
-    public int getIdFerramenta() {
-        return idFerramenta;
-    }
+	// Construtor com valor false no boolean emprestada já que toda ferramenta começa não emprestada
+	public Ferramenta(int idFerramenta, String nomeFerramenta, String marca, double custo) {
+		this.idFerramenta = idFerramenta;
+		this.nomeFerramenta = nomeFerramenta;
+		this.marca = marca;
+		this.custo = custo;
+		this.emprestada = false;
+	}
 
-    public void setIdFerramenta(int idFerramenta) {
-        this.idFerramenta = idFerramenta;
-    }
+	public Ferramenta(int idFerramenta, String nomeFerramenta, String marca, double custo, boolean emprestada) {
+		this.idFerramenta = idFerramenta;
+		this.nomeFerramenta = nomeFerramenta;
+		this.marca = marca;
+		this.custo = custo;
+		this.emprestada = emprestada;
+	}
 
-    public String getNomeFerramenta() {
-        return nomeFerramenta;
-    }
+	public int getIdFerramenta() {
+		return idFerramenta;
+	}
 
-    public void setNomeFerramenta(String nomeFerramenta) {
-        this.nomeFerramenta = nomeFerramenta;
-    }
+	public void setIdFerramenta(int idFerramenta) {
+		this.idFerramenta = idFerramenta;
+	}
 
-    public String getMarca() {
-        return marca;
-    }
+	public String getNomeFerramenta() {
+		return nomeFerramenta;
+	}
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+	public void setNomeFerramenta(String nomeFerramenta) {
+		this.nomeFerramenta = nomeFerramenta;
+	}
 
-    public double getCusto() {
-        return custo;
-    }
+	public String getMarca() {
+		return marca;
+	}
 
-    public void setCusto(double custo) {
-        this.custo = custo;
-    }
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
 
-    public boolean isEmprestada() {
-        return emprestada;
-    }
+	public double getCusto() {
+		return custo;
+	}
 
-    public void setEmprestada(boolean emprestada) {
-        this.emprestada = emprestada;
-    }
-    
-    @Override
-    public String toString() {
-        return "idFerramenta=" + idFerramenta + ", nomeFerramenta=" + nomeFerramenta + ", marca=" + marca + ", custo=" + custo + ", emprestada=" + emprestada;
-    }
+	public void setCusto(double custo) {
+		this.custo = custo;
+	}
 
+	public boolean isEmprestada() {
+		return emprestada;
+	}
+
+	public void setEmprestada(boolean emprestada) {
+		this.emprestada = emprestada;
+	}
+
+	@Override
+	public String toString() {
+		return "idFerramenta=" + idFerramenta + ", nomeFerramenta=" + nomeFerramenta + ", marca=" + marca + ", custo=" + custo + ", emprestada=" + emprestada;
+	}
+
+	// Os métodos a seguir referenciam implementacoes futuras da classe DAO
+	// Pro netbeans não reclamar, mantenha esse trecho comentado por enquanto
+	
+	/*
+	public ArrayList<Ferramenta> getMinhaLista() {
+		return dao.getMinhaLista();
+	}
+
+	public boolean insertFerramentaBD(String nome, String marca, int custo, boolean emprestada) {
+		int id = this.maiorID() + 1;
+		Ferramenta objeto = new Ferramenta(id, nome, marca, custo, emprestada);
+		dao.insertFerramentaBD(objeto);
+		return true;
+	}
+
+	public boolean deleteFerramentaBD(int id) {
+		dao.deleteFerramentaBD(id);
+		return true;
+	}
+
+	public boolean updateFerramentaBD(int id, String nome, String marca, int custo, boolean emprestada) {
+		Ferramenta objeto = new Ferramenta(id, nome, marca, custo, emprestada);
+		dao.updateFerramentaBD(objeto);
+		return true;
+	}
+
+	public Ferramenta carregaFerramenta(int id) {
+		return dao.carregaFerramenta(id);
+	}
+
+	public int maiorID() {
+		return dao.maiorID();
+	}
+	
+	*/
+	
 }
