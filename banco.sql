c701fe193cd8c7cf2cb4ac36b02021f916316da2
JP-Becker <jpbecker20@gmail.com>
2024-05-18 11:41:56 -0300

    feat: alterando e adicionando alguns inserts a mais no script do banco mySQL

--- banco.sql
+++ banco.sql
@@ -16,7 +16,7 @@ CREATE TABLE tb_amigos (
     PRIMARY KEY(idAmigo)
 );
 
-// criadno tabela das ferramentas
+// criando tabela das ferramentas
 CREATE TABLE tb_ferramentas (
     idFerramenta INTEGER NOT NULL,
     nomeFerramenta VARCHAR(100),
@@ -27,8 +27,11 @@ CREATE TABLE tb_ferramentas (
 );
 
 
-// INSERINDO O PRIMEIRO AMIGO PARA TESTAR
-INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (1,"João","48999666847","0",0);
+// inserindo amigos na BD
+INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (1,"João","48999666847",0,0);
+INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (2,"José","48991234589",0,0);
+INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (3,"Maria","489923452145",0,0);
 
-// INSERINDO UMA FERRAMENTA
-INSERT INTO tb_ferramentas(idFerramenta,nomeFerramenta,marca,custo,emprestada) VALUES (1,"chave de fenda","philips","9.99", false);
+// inserindo ferramentas
+INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo, emprestada) VALUES (1, 'chave de fenda', 'philips', 9.99, false);
+INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo, emprestada) VALUES (2, 'furadeira', 'samsung', 109.99, false);
