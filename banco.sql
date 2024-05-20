94136cef6b4499cd90bd28ee047715a96ea665e1
JP Becker <jpbecker20@gmail.com>
2024-05-20 10:53:54 -0300

    feat: criando tb_emprestimos e inserindo alguns dados nela

--- banco.sql
+++ banco.sql
@@ -26,6 +26,16 @@ CREATE TABLE tb_ferramentas (
     PRIMARY KEY(idFerramenta)
 );
 
+# criando tabela dos emprestimos 
+CREATE TABLE tb_emprestimos (
+    idEmprestimo INTEGER NOT NULL,
+    dataEmprestimo DATE,
+    dataDevolucao DATE,
+    pendente BOOLEAN,
+    PRIMARY KEY(idEmprestimo)
+);
+
+
 
 # inserindo amigos na BD
 INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (1,"João","48999666847",0,0);
@@ -35,3 +45,8 @@ INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAt
 # inserindo ferramentas
 INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo, emprestada) VALUES (1, 'chave de fenda', 'philips', 9.99, false);
 INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo, emprestada) VALUES (2, 'furadeira', 'samsung', 109.99, false);
+
+# inserindo emprestimos na tabela de emprestimos
+INSERT INTO tb_emprestimos(idEmprestimo,dataEmprestimo,dataDevolucao,pendente) VALUES (1,NULL,NULL,true);
+INSERT INTO tb_emprestimos(idEmprestimo,dataEmprestimo,dataDevolucao,pendente) VALUES (2,NULL,NULL,true);
+INSERT INTO tb_emprestimos(idEmprestimo,dataEmprestimo,dataDevolucao,pendente) VALUES (3,NULL,NULL,true);
