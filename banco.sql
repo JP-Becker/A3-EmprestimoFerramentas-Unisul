d4f438ceaba72547429ba924c046edf6db787904
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-05-20 10:50:45 -0300

    fix: Correção dos comentários
    Comentários alterados do formato "//" para o formato "#", compatível com o SQL

--- banco.sql
+++ banco.sql
@@ -1,4 +1,4 @@
-// FEITO POR JOÃO
+# FEITO POR JOÃO
 CREATE DATABASE IF NOT EXISTS db_a3;
 
 USE db_a3;
@@ -6,7 +6,7 @@ USE db_a3;
 DROP TABLE IF EXISTS tb_amigos;
 
 
-// criando tabela dos amigos
+# criando tabela dos amigos
 CREATE TABLE tb_amigos (
     idAmigo INTEGER NOT NULL,
     nomeAmigo VARCHAR(100),
@@ -16,7 +16,7 @@ CREATE TABLE tb_amigos (
     PRIMARY KEY(idAmigo)
 );
 
-// criando tabela das ferramentas
+# criando tabela das ferramentas
 CREATE TABLE tb_ferramentas (
     idFerramenta INTEGER NOT NULL,
     nomeFerramenta VARCHAR(100),
@@ -27,11 +27,11 @@ CREATE TABLE tb_ferramentas (
 );
 
 
-// inserindo amigos na BD
+# inserindo amigos na BD
 INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (1,"João","48999666847",0,0);
 INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (2,"José","48991234589",0,0);
 INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone,emprestimosTotais,emprestimosAtivos) VALUES (3,"Maria","489923452145",0,0);
 
-// inserindo ferramentas
+# inserindo ferramentas
 INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo, emprestada) VALUES (1, 'chave de fenda', 'philips', 9.99, false);
 INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo, emprestada) VALUES (2, 'furadeira', 'samsung', 109.99, false);
