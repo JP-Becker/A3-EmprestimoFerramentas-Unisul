f3c82cef875b1fc9f8ce98acc49fa85304415ddd
Hiago de Mendonça <163073487+HiagoDeMendonca@users.noreply.github.com>
2024-05-21 12:04:05 -0300

    feat: Implementação do gerenciamento de ferramentas
    Janela de gerenciamento de ferramentas implementada com sua janela e funcionalidades
    (Por ora, o gerenciamento não altera o banco de dados. Será necessária a criação de métodos no DAO para isso. Quando isso foi feito, dois trechos poderão ser descomentados para aplicar a funcionalidade)
    
    Adição da classe "Mensagem" para tratar exceções em FrmGerenciarFerramenta e outras classes

--- src/main/java/visao/FrmGerenciarFerramenta.java
+++ src/main/java/visao/FrmGerenciarFerramenta.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package visao;
 
-/**
- *
- * @author 1072416369
- */
+import modelo.Ferramenta;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 public class FrmGerenciarFerramenta extends javax.swing.JFrame {
+
+    private Ferramenta objetoferramenta;
 
     /**
      * Creates new form FrmGerenciarFerramenta
      */
     public FrmGerenciarFerramenta() {
         initComponents();
+        this.objetoferramenta = new Ferramenta(); // carrega objetoferramenta de ferramenta
+        this.carregaTabela();
     }
 
     /**
@@ -26,35 +27,51 @@ public class FrmGerenciarFerramenta extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        JPGerenciaFerramentas = new javax.swing.JTable();
         JBApagar = new javax.swing.JButton();
         JBAlterar = new javax.swing.JButton();
         JBCancelar = new javax.swing.JButton();
-        JBSalvar = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable = new javax.swing.JTable();
+        JTFCusto = new javax.swing.JTextField();
+        JTFNome = new javax.swing.JTextField();
+        JTFMarca = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel1.setText("Gerência ferramentas");
-
-        JPGerenciaFerramentas.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        JPGerenciaFerramentas.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {},
-                {},
-                {},
-                {}
-            },
-            new String [] {
-
-            }
-        ));
-        jScrollPane1.setViewportView(JPGerenciaFerramentas);
+        jLabel1.setText("Gerência ferramentas:");
 
         JBApagar.setText("Apagar");
+        JBApagar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBApagarActionPerformed(evt);
+            }
+        });
 
         JBAlterar.setText("Alterar");
+        JBAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBAlterarActionPerformed(evt);
+            }
+        });
 
         JBCancelar.setText("Cancelar");
         JBCancelar.addActionListener(new java.awt.event.ActionListener() {
@@ -63,52 +80,238 @@ public class FrmGerenciarFerramenta extends javax.swing.JFrame {
             }
         });
 
-        JBSalvar.setText("Salvar");
+        jTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Id", "Nome", "Marca", "Custo"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jTable.getTableHeader().setReorderingAllowed(false);
+        jTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jTableMousePressed(evt);
+            }
+        });
+        jScrollPane3.setViewportView(jTable);
+
+        JTFCusto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JTFCustoActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Nome:");
+
+        jLabel3.setText("Marca:");
+
+        jLabel4.setText("Custo:");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(72, 72, 72)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(JBApagar, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
-                            .addComponent(JBCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE))
-                        .addGap(12, 12, 12)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(JBSalvar, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)
-                            .addComponent(JBAlterar, javax.swing.GroupLayout.DEFAULT_SIZE, 85, Short.MAX_VALUE)))
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(128, Short.MAX_VALUE))
+                        .addGap(34, 34, 34)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 312, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(JTFCusto, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(JBCancelar)
+                                .addGap(29, 29, 29)
+                                .addComponent(JBApagar, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(JBAlterar, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(JTFMarca, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel2)
+                            .addComponent(JTFNome, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))))
+                .addContainerGap(19, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(30, 30, 30)
+                .addGap(19, 19, 19)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 132, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(25, 25, 25)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 154, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addGap(2, 2, 2)
+                .addComponent(JTFNome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(3, 3, 3)
+                .addComponent(jLabel3)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(JTFMarca, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel4)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(JTFCusto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(JBAlterar)
-                    .addComponent(JBApagar))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(JBSalvar)
-                    .addComponent(JBCancelar))
-                .addContainerGap(63, Short.MAX_VALUE))
+                    .addComponent(JBCancelar)
+                    .addComponent(JBApagar)
+                    .addComponent(JBAlterar))
+                .addGap(15, 15, 15))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void JBCancelarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBCancelarActionPerformed
-        // TODO add your handling code here:
         this.dispose();
     }//GEN-LAST:event_JBCancelarActionPerformed
+
+    private void JTFCustoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JTFCustoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JTFCustoActionPerformed
+
+    private void jTableMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTableMousePressed
+        if (this.jTable.getSelectedRow() != -1) {
+            String nome = this.jTable.getValueAt(this.jTable.getSelectedRow(), 1).toString();
+            String marca = this.jTable.getValueAt(this.jTable.getSelectedRow(), 2).toString();
+            String custo = this.jTable.getValueAt(this.jTable.getSelectedRow(), 3).toString();
+
+            this.JTFNome.setText(nome);
+            this.JTFCusto.setText(custo);
+            this.JTFMarca.setText(marca);
+        }
+    }//GEN-LAST:event_jTableMousePressed
+
+    private void JBApagarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBApagarActionPerformed
+        try {
+            // validando dados da interface gráfica.
+            int id = 0;
+            if (this.jTable.getSelectedRow() == -1) {
+                throw new Mensagem("Primeiro Selecione um Aluno para APAGAR");
+            } else {
+                id = Integer.parseInt(this.jTable.getValueAt(this.jTable.getSelectedRow(), 0).toString());
+            }
+
+            // retorna 0 -> primeiro botão | 1 -> segundo botão | 2 -> terceiro botão
+            int respostaUsuario = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja apagar esta ferramenta ?");
+
+            if (respostaUsuario == 0) {// clicou em SIM
+                // envia os dados para o Aluno processar
+
+                // ATENCAO: if abaixo comentado por não haver deleteFerramentaBD ainda
+                // NAO ESQUECER DE DESCOMENTAR DEPOIS
+
+                //if (this.objetoferramenta.deleteFerramentaBD(id)) {
+                // limpa os campos
+                this.JTFNome.setText("");
+                this.JTFCusto.setText("");
+                this.JTFMarca.setText("");
+                JOptionPane.showMessageDialog(rootPane, "Ferramenta apagada com Sucesso!");
+                //}
+            }
+            // atualiza a tabela.
+            System.out.println(this.objetoferramenta.getMinhaLista().toString());
+        } catch (Mensagem erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        } finally {
+            // atualiza a tabela.
+            carregaTabela();
+        }
+    }//GEN-LAST:event_JBApagarActionPerformed
+
+    private void JBAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBAlterarActionPerformed
+        try {
+            // recebendo e validando dados da interface grafica.
+            int id = 0;
+            String nome = "";
+            String marca = "";
+            double custo = 0.0;
+
+            if (this.JTFNome.getText().length() <= 0) {
+                throw new Mensagem("Nome não pode estar vazio.");
+            } else {
+                nome = this.JTFNome.getText();
+            }
+            
+            if (this.JTFMarca.getText().length() <= 0) {
+                throw new Mensagem("Marca não pode estar vazia.");
+            } else {
+                nome = this.JTFMarca.getText();
+            }
+
+            custo = Double.parseDouble(this.JTFCusto.getText());
+
+            if (this.jTable.getSelectedRow() == -1) {
+                throw new Mensagem("Primeiro Selecione um Aluno para Alterar");
+            } else {
+                id = Integer.parseInt(this.jTable.getValueAt(this.jTable.getSelectedRow(), 0).toString());
+            }
+
+            // envia os dados para o Aluno processar
+            
+            // ATENCAO: if abaixo comentado por não haver deleteFerramentaBD ainda
+            // NAO ESQUECER DE DESCOMENTAR DEPOIS
+
+            //if (this.objetoferramenta.updateAlunoBD(id, nome, marca, custo)) {
+                // limpa os campos
+                this.JTFNome.setText("");
+                this.JTFMarca.setText("");
+                this.JTFCusto.setText("");
+                JOptionPane.showMessageDialog(null, "Ferramenta alterada com sucesso!");
+
+            //}
+            
+            // Exibe no console o aluno cadastrado
+            System.out.println(this.objetoferramenta.getMinhaLista().toString());
+        } catch (Mensagem erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        } catch (NumberFormatException erro2) {
+            JOptionPane.showMessageDialog(null, "Informe um número válido.");
+        } finally {
+            // atualiza a tabela.
+            carregaTabela();
+        }
+    }//GEN-LAST:event_JBAlterarActionPerformed
+
+    public void carregaTabela() {
+        DefaultTableModel modelo = (DefaultTableModel) this.jTable.getModel();
+        modelo.setNumRows(0); // Posiciona na primeira linha da tabela
+        // Carrega a lista de objetos aluno
+        ArrayList<Ferramenta> minhaLista = objetoferramenta.getMinhaLista();
+        for (Ferramenta a : minhaLista) {
+            modelo.addRow(new Object[]{
+                a.getIdFerramenta(),
+                a.getNomeFerramenta(),
+                a.getMarca(),
+                a.getCusto(),});
+        }
+    }
 
     /**
      * @param args the command line arguments
@@ -149,9 +352,16 @@ public class FrmGerenciarFerramenta extends javax.swing.JFrame {
     private javax.swing.JButton JBAlterar;
     private javax.swing.JButton JBApagar;
     private javax.swing.JButton JBCancelar;
-    private javax.swing.JButton JBSalvar;
-    private javax.swing.JTable JPGerenciaFerramentas;
+    private javax.swing.JTextField JTFCusto;
+    private javax.swing.JTextField JTFMarca;
+    private javax.swing.JTextField JTFNome;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable;
+    private javax.swing.JTable jTable2;
     // End of variables declaration//GEN-END:variables
 }
