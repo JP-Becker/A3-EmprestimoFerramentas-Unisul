dfbad10726e8d1c54b9c7388ba763a07871c36ea
JP Becker <jpbecker20@gmail.com>
2024-05-28 10:40:06 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,11 +1,6 @@
-CREATE DATABASE IF NOT EXISTS db_a3;
-
-USE db_a3;
-
 DROP TABLE IF EXISTS tb_amigos;
 DROP TABLE IF EXISTS tb_ferramentas;
 DROP TABLE IF EXISTS tb_emprestimos;
-
 
 # criando tabela dos amigos
 CREATE TABLE tb_amigos (
@@ -27,24 +22,10 @@ CREATE TABLE tb_ferramentas (
 # criando tabela dos emprestimos 
 CREATE TABLE tb_emprestimos (
     idEmprestimo INTEGER NOT NULL,
+    idAmigo INTEGER,
+    idFerramenta INTEGER,
     dataEmprestimo DATE,
     dataDevolucao DATE,
     pendente BOOLEAN,
     PRIMARY KEY(idEmprestimo)
 );
-
-
-
-# inserindo amigos na BD
-INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone) VALUES (1,"João","48999666847");
-INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone) VALUES (2,"José","48991234589");
-INSERT INTO tb_amigos(idAmigo,nomeAmigo,telefone) VALUES (3,"Maria","489923452145");
-
-# inserindo ferramentas
-INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo) VALUES (1, 'chave de fenda', 'philips', 9.99);
-INSERT INTO tb_ferramentas(idFerramenta, nomeFerramenta, marca, custo) VALUES (2, 'furadeira', 'samsung', 109.99);
-
-# inserindo emprestimos na tabela de emprestimos
-INSERT INTO tb_emprestimos(idEmprestimo,dataEmprestimo,dataDevolucao,pendente) VALUES (1,NULL,NULL,true);
-INSERT INTO tb_emprestimos(idEmprestimo,dataEmprestimo,dataDevolucao,pendente) VALUES (2,NULL,NULL,true);
-INSERT INTO tb_emprestimos(idEmprestimo,dataEmprestimo,dataDevolucao,pendente) VALUES (3,NULL,NULL,true);
